5542b9c56ba2f5c7f2461204c83e4a5bfa31735c
Jay Bonthius <jaybonthius@gmail.com>
2025-07-26 14:34:29 -0700

    set tab stop

--- init.lua
+++ init.lua
@@ -107,6 +107,9 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
 
+-- Set tab width to 4 spaces
+vim.o.tabstop = 4
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
