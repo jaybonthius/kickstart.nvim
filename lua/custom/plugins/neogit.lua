6a8120b5810aa7eafa56164377dff4891a5fc57c
Jay Bonthius <jaybonthius@gmail.com>
2025-07-30 09:40:14 -0700

    add neogit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    local neogit = require 'neogit'
+    neogit.setup {}
+    vim.keymap.set('n', '<leader>gs', neogit.open, { desc = '[G]it [S]tatus (Neogit)' })
+  end,
+}
+
